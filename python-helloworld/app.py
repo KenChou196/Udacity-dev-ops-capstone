e26d170e1dd1ca592498fa3212199e83c71c9465
kynk1 <metal.bird.13@gmail.com>
2023-05-29 10:27:27 +0700

    test roll back

--- python-helloworld/app.py
+++ python-helloworld/app.py
@@ -30,7 +30,7 @@ def metrics():
 def hello():
     app.logger.info('Main request successfull')
 
-    return "Hello World!"
+    return "Hooray, Deploy is success"
 
 if __name__ == "__main__":
     ## stream logs to a file
